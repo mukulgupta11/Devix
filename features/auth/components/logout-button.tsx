94391d117dd7bb6c45b7395f1eb89656d6436f47
mukulgupta11 <mukulgupta3264@gmail.com>
2026-04-27 17:35:04 +0530

    Fix: use server action for logout to resolve production issue (closes #1)

--- features/auth/components/logout-button.tsx
+++ features/auth/components/logout-button.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import { LogoutButtonProps } from '../types'
-import { useRouter } from 'next/navigation'
-import { signOut } from 'next-auth/react';
+import { logout } from '@/features/auth/actions';
 
 const LogoutButton = ({children}:LogoutButtonProps) => {
-    const router = useRouter();
     const onLogout = async()=>{
-        await signOut({ redirect: false });
-        router.push("/");
+        await logout();
     }
   return (
     <span className='cursor-pointer' onClick={onLogout}>
